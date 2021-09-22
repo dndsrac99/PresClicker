35b18be12b0f7d8c9ef83fa4a67af5ed707d9ccf
dndsrac99 <dnd.srac@icloud.com>
2021-09-22 08:39:36 +0200

    -initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-btn height="100%" @click="orbi =!orbi"> <v-img src="..\assets\a.png"></v-img></v-btn>
+    <v-overlay :value="orbi">
+      <v-btn @click="orbi = !orbi"> <v-img src="..\assets\b.png"></v-img> </v-btn>
+       </v-overlay>
+    
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+
+    data: () => ({
+      orbi: false,
+      }),
+
+      watch: {
+        
+      }
+  }
+</script>
